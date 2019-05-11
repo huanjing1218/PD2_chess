d6764ac02ce0d5058f5d52f8500a25e2c4584999
environment <huanjing881218@gmail.com>
2019-05-12 02:49:39 +0800

    resize mainwindow

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+    w.resize(560,630);
+    w.setWindowTitle("CHESS GAME");
     w.show();
 
     return a.exec();
